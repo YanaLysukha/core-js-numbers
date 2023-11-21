12efe00305e21b427d75291bdb8b267a5f4aa398
YanaLysukha <yanina.lysukha@gmail.com>
2023-11-21 23:09:45 +0100

    feat: implement get count of odd numb function

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -659,8 +659,9 @@ function getHypotenuse(a, b) {
  * 10 => 5
  * 15 => 8
  */
-function getCountOfOddNumbers(/* number */) {
-  throw new Error('Not implemented');
+function getCountOfOddNumbers(number) {
+  const positiveNum = Math.abs(number);
+  return Math.ceil(positiveNum / 2);
 }
 
 module.exports = {
